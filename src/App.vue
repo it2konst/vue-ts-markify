cb8d2af9aee9f6f0e3eb636207b4c9b047e2e9fa
Konst Berg <>
2025-06-08 19:50:26 +0700

    refactor: simplify App.vue structure and remove unused header elements; clean up base.css by removing min-height from body

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+  <div class="app">
+    <nav class="nav">Меню</nav>
+    <main>Контент</main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.app {
+  display: flex;
+  min-height: calc(100vh - 140px);
+  gap: 200px;
+  max-width: 1450px;
+  margin: 140px auto 0 auto;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.nav {
+  min-width: 400px;
 }
 </style>
