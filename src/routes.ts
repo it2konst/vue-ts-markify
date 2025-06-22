bbc5aaf48227e77b8dadc804dfd1d070c8752fb1
Konst Berg <>
2025-06-22 17:55:41 +0700

    feat: Refactor App structure and implement routing; add AuthView and MainView components

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import AuthView from './views/AuthView.vue';
+import MainView from './views/MainView.vue';
+
+export const router = createRouter({
+  routes: [
+    {
+      path: '/',
+      component: AuthView,
+    },
+    {
+      path: '/main',
+      component: MainView,
+    },
+  ],
+  history: createWebHistory(),
+});
